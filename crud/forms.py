0d735c4395b9433baea8b5318c2afdd39beeb601
Pralhad17 <113904436+Pralhad17@users.noreply.github.com>
2023-08-25 14:33:37 +0530

    Update forms.py
    Remove help text

--- crud/forms.py
+++ crud/forms.py
@@ -13,6 +13,14 @@ class CreateForm(UserCreationForm):
         model = User
         fields = ['username','email','password1','password2']
 
+    # Remove help Text
+    def __init__(self, *args, **kwargs) :
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text =None
+        self.fields['email'].help_text = None
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
+
 
 #  Login User Form
 
